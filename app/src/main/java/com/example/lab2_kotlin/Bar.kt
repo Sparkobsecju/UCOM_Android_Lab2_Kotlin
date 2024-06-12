fd9e964a35e3ca263b9e4e6703ed603db5c20604
Sivan Chen <ryan1825114@gmail.com>
2024-06-12 13:59:14 +0800

    init

---
+++ app/src/main/java/com/example/lab2_kotlin/Bar.kt
@@ -0,0 +1,11 @@
+package com.example.lab2_kotlin
+
+import android.view.View
+import android.widget.Toast
+
+class Bar(private val context: SecondActivity) : View.OnClickListener {
+    override fun onClick(v: View?) {
+        Toast.makeText(context,
+            "Button 4 clicked", Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/example/lab2_kotlin/SecondActivity.kt
+++ app/src/main/java/com/example/lab2_kotlin/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab2_kotlin
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -16,6 +17,40 @@ class SecondActivity: AppCompatActivity(), View.OnClickListener {
         button2.setOnClickListener(this)
         val button3: Button = findViewById(R.id.button3)
         button3.setOnClickListener(Foo())
+        val button4: Button = findViewById(R.id.button4)
+        button4.setOnClickListener(Bar(this))
+        val button5: Button = findViewById(R.id.button5)
+        button5.setOnClickListener { v ->
+            Toast.makeText(
+                this, "button 5 clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        val button6: Button = findViewById(R.id.button6)
+        button6.setOnClickListener{
+            Toast.makeText(this, "Button 6 clicked",
+                Toast.LENGTH_SHORT).show()
+        }
+        val button7: Button = findViewById(R.id.button7)
+        button7.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@SecondActivity,
+                    "Button 7 clicked", Toast.LENGTH_SHORT).show()
+            }
+        })
+        val button8: Button = findViewById(R.id.button8)
+        button8.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                v!!.setBackgroundColor(Color.BLUE)
+            }
+        })
+        val button9: Button = findViewById(R.id.button9)
+        button9.setOnClickListener{ v ->
+            val button: Button = v as Button
+            button.text = "1234567"
+            button.setBackgroundColor(Color.BLACK)
+        }
+
     }
 
     inner class Foo : View.OnClickListener {
