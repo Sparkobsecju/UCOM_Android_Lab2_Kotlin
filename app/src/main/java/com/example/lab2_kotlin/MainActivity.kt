e2773dc85ef53a4e279638836b9f939648281a95
Sivan Chen <ryan1825114@gmail.com>
2024-06-12 13:59:14 +0800

    steps to make phone call and deal with permission

--- app/src/main/java/com/example/lab2_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/lab2_kotlin/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.lab2_kotlin
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val CALL_PHONE_PERMISSION_CHECK = 4321
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,4 +39,27 @@ class MainActivity : AppCompatActivity() {
         intent.data = Uri.parse("http://www.yahoo.com.tw")
         startActivity(intent)
     }
+
+    fun doPhone(v: View) {
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(this,"need to set permission", Toast.LENGTH_SHORT).show()
+        } else {
+            realCallPhone()
+        }
+    }
+
+    private fun realCallPhone() {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.data = Uri.parse("tel:1234565")
+        startActivity(intent)
+    }
+
 }
